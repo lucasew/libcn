50e476b078705b3a68286e1b3edc82d421bcd3e3
Lucas59356 <lucas59356@gmail.com>
2019-05-21 12:16:28 +0000

    Adicionada interface cli do programa

--- cholesky/ops.c
+++ cholesky/ops.c
@@ -2,26 +2,30 @@
 #define CN_CHOLESKY
 #include "../matriz/ops.c"
 #include "../matriz/struct.c"
+#include "../status.h"
 #include <math.h>
 int cn_cholesky__check(struct cn_matriz *this) {
     if (!cn_matriz__is_quadrada(this))
-        return 0;
+        return ERROR;
+    if (!cn_matriz__is_simetrica(this))
+        return ERROR;
     int i;
     for (i = 1; i < this->tam_i; i++) {
         double det;
         struct cn_matriz menores;
         if (cn_matriz__get_menor_principal(this, &menores, i) == ERROR)
-            return 0;
+            return ERROR;
         if (cn_matriz__get_determinante(&menores, &det) == ERROR)
-            return 0;
+            return ERROR;
+        cn_matriz__destroy(&menores);
         if (det <= 0)
-            return 0;
+            return ERROR;
     }
-    return 1;
+    return SUCESS;
 }
 
 int cn_cholesky__calc(struct cn_matriz *this, struct cn_matriz *res) {
-    if (!cn_cholesky__check(this))
+    if (cn_cholesky__check(this) == ERROR)
         return ERROR;
     cn_matriz__init(res, this->tam_i, this->tam_j);
 
---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include "./matriz/ops.c"
+#include "./matriz/struct.c"
+#include "./cholesky/ops.c"
+#include "./status.h"
+
+#define THROW(cond, errmsg) \
+    if (cond) { \
+        printf("%s\n", errmsg); \
+        return ERROR; \
+    }
+
+int cn_main__parse_matriz(struct cn_matriz *mtx, int argc, char **argv) {
+    THROW(argc < 2, "ERRO: Parâmetros insuficientes. Esperado tam_i tam_j valores...");
+    int tam_i = atoi(argv[0]);
+    int tam_j = atoi(argv[1]);
+    /* THROW(!tam_i && !tam_j, "ERRO: Valores inválidos fornecidos para tamanho."); */
+    argc -= 2;
+    argv += 2;
+    cn_matriz__init(mtx, tam_i, tam_j);
+    int i;
+    for (i = 0; i < tam_i*tam_j; i++) {
+        THROW(argc == 0, "Valores insuficientes para preencher a matriz.");
+        double vlr = atof(argv[0]);
+        /* THROW(vlr == 0.0, "ERRO: Valor inválido fornecido para a matriz."); */
+        mtx->vet[i] = vlr;
+        argc--;
+        argv++;
+    }
+    return SUCESS;
+}
+
+int main(int argc, char **argv) {
+    for (int i = 0; i < argc; i++)
+        printf("ARGV: %s\n", argv[i]);
+    struct cn_matriz mtx, res;
+    mtx.vet = NULL;
+    THROW(argc < 2, "ERRO: Argumentos insuficientes");
+    char *op = argv[1];
+    printf("%s\n", op);
+    cn_main__parse_matriz(&mtx, argc - 2, argv + 2);
+    if (!strncmp(op, "cholesky", 1)) {
+        THROW(cn_cholesky__calc(&mtx, &res) == ERROR, "Erro ao calcular cholesky: Critérios podem não ter sido satisfeitos\n");
+        cn_matriz__prettyprint(&res);
+        cn_matriz__destroy(&mtx);
+        cn_matriz__destroy(&res);
+    }
+    if (!strncmp(op, "print", 1)) {
+        cn_matriz__prettyprint(&mtx);
+        cn_matriz__destroy(&mtx);
+    }
+    return SUCESS;
+}
